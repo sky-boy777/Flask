033353af819e59eec290514ff26a2735f7a192bc
sky <1251779123@qq.com>
2020-08-29 21:44:32 +0800

    增加

--- flask蓝图/apps/user/view.py
+++ flask蓝图/apps/user/view.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template, url_for, request
 from .models import User
 from ext import db
+import hashlib
 
 # 相当于app
 user_bp = Blueprint('user', __name__)
@@ -10,30 +11,23 @@ user_bp = Blueprint('user', __name__)
 def register():
     '''注册'''
     if request.method == 'POST':
+        # 获取
+        username = request.form.get('username')
+        password = request.form.get('password')
         # 注册
-        # username = request.form.get('username')  # post
         user = User()
-        user.username = '张三'
-        user.password = '123456'
-
-        # 创建一个新用户对象
+        user.username = username
+        user.password = hashlib.sha256(password.encode('utf-8'))  # 加密，先编码转换才能使用加密
+        # 添加并保存
         db.session.add(user)
-        # 提交保存
         db.session.commit()
         return '注册成功'
-    # username = request.args.get('username')  # get
-    user = User()
-    user.username = '张三'
-    user.password = '123456'
 
-    # 创建一个新用户对象
-    db.session.add(user)
-    # 提交保存
-    db.session.commit()
+    # username = request.args.get('username')  # get
     return render_template('user/register.html')  # 渲染模板
 
 @user_bp.route('/login', methods=['GET', 'POST'], endpoint='login')  # 未设置endpoint默认使用函数名login
 def login():
-    print(url_for('user.login'))  # 用户模块下的登录页, /login
+
     return '用户登录'
 
